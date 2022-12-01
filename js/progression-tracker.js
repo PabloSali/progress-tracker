e185e7455176c37c62fc78d97057871957bf60f4
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-01 08:16:37 -0600

    Created Operation Plan wihtin the js document and added tailwind CDN

---
+++ js/progression-tracker.js
@@ -0,0 +1,38 @@
+"use strict"
+
+
+
+/* -----------------------------------------------------------------
+                           Operation Plan
+- Create a dashboard to display all important data
+    - Overall Caliber Score
+        - Need a Variable that assigned to the overall score
+            - Use function to add and feed data to variable
+        - Need to use dom manip. to push to HTML
+
+    - Skills that are close to next level
+        - Create function that is called once a skill is within a certain percentage of being "leveled up"
+        - Create a better word for "leveled up" (maybe something with caliber)
+        - Compare variables within that percentage and display on the three closest ones
+
+    - Skills that are deteriorating
+        - Create function that is called once a certain amount of degradation is met
+
+    - Skills that are marked as favorite
+        - Button that adds this skill to the HTML displaying info
+
+- Create functionality that establishes core service of Wherewithal
+    - Create Deterioration
+        - Create variable that defines how fast a skill deteriorates
+        - Assign value of degradation within the skills calculation function
+        - Research skills degredation
+
+    - Create Progression
+        - Create skill breakdowns, so they can feed multiple skills
+        -
+
+    - Create Parent Attributes that house skills
+        -
+
+----------------------------------------------------------------- */
+
